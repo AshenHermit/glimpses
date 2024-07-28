ec401aa3c892d50b60759243162ee86705079229
AshenHermit <nameless.voice.x@gmail.com>
2024-07-28 21:34:50 +0300

    fix: env api variable

--- src/app/variables.ts
+++ src/app/variables.ts
@@ -1,6 +1,6 @@
 export const NEXT_API =
   process.env.NODE_ENV === "production"
-    ? process.env.NEXT_PUBLIC_API_NEXT_URL
+    ? process.env.NEXT_PUBLIC_API_URL
     : `http://localhost:${process.env.PORT || 3000}/api/`
 export const REVALIDATE_INTERVAL = Number(process.env.REVALIDATE_INTERVAL) || 180
 export const JWT_SECRET = process.env.NEXT_JWT_SECRET || "key"
